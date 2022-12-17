96e21292dff08e359440b009021ee6428f4caa5c
netanyaj <netanyajaison@gmail.com>
2022-12-17 19:32:53 +0530

    auth and tweet stream

---
+++ frontend/src/components/spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className="ui active centered large inline loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
