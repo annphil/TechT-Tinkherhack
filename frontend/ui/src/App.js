7ac789cdeb6431fc604b120086476d521d909427
NavyaVarghese <84275768+NavyaVarghese@users.noreply.github.com>
2022-12-18 01:07:50 +0530

    modified ui

--- frontend/ui/src/App.js
+++ frontend/ui/src/App.js
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 import './App.css';
 
 
@@ -8,19 +6,23 @@ import {BrowserRouter as Router} from 'react-router-dom';
 export default function App() {
   return (
     <>
-    <div className="center">
+   <div className="begn">  
     <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png" alt='logo' className="pic" /><Router>
-    
-    <h4>To use this app you must be<br></br> logged in to twitter</h4>  
+    <div className="center">
+    <h4>To use this app you must be&nbsp;</h4><br></br><h4> logged in to twitter</h4> 
+    </div> 
       <div>
        
        
-
+        <div className="center2">
         <a href="https://twitter.com/i/flow/login" target="_blank" rel="noreferrer">
-          <button>Login</button>
+          <button className="class2">Login</button>
         </a>
+        </div>
       </div>
       
-    </Router></div></>
+    </Router>
+    </div></>
+    
   );
 }
